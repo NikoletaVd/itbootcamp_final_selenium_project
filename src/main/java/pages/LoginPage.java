7624e4f65dde64294d75bae3c085a0a2603c34e9
Nikoleta Vdovenko <nikoleta.nojic@gmail.com>
2023-02-14 13:15:49 +0100

    Update Login page

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,11 +1,25 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.security.PublicKey;
 
 public class LoginPage extends BasePage{
     public LoginPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
+    }
+
+    public WebElement getEmailInput() {
+        return driver.findElement(By.name("email"));
+    }
+    public WebElement getPasswordInput() {
+        return driver.findElement(By.name("password"));
+    }
+    public WebElement getLoginButton() {
+        return driver.findElement(By.xpath("//button[@type='submit']"));
     }
 
 }
